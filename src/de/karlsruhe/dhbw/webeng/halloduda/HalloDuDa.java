6dcfa11f110f2d0ee686969b22e75e0797659d7a
Daria Seledtsova <d.seledtsova@gmail.com>
2017-06-10 21:31:40 +0200

    task1: clean(er) version using service method

--- src/de/karlsruhe/dhbw/webeng/halloduda/HalloDuDa.java
+++ src/de/karlsruhe/dhbw/webeng/halloduda/HalloDuDa.java
@@ -14,28 +14,21 @@ import java.io.PrintWriter;
  */
 @WebServlet(value = "/halloduda")
 public class HalloDuDa extends HttpServlet {
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        PrintWriter printWriter = resp.getWriter();
-        StringBuffer response = new StringBuffer();
-        response.append("<!DOCTYPE html>");
-        response.append("<html><head></head><title> Hallo du da </title>");
-        response.append("<body><form action=\"/halloduda\" method=\"post\"> Wie ist dein Name? <input type=\"text\" name=\"firstname\">");
-        response.append("<br> <input type=\"submit\" value=\"Absenden\"> </form></body> </html>");
-        printWriter.print(response);
-    }
 
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String name = req.getParameter("firstname");
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         PrintWriter printWriter = resp.getWriter();
         StringBuffer response = new StringBuffer();
-        response.append("<!DOCTYPE html>");
-        response.append("<html><head></head><title> Hallo ");
-        response.append(name);
-        response.append("</title>");
-        response.append("<body><form action=\"/halloduda\" method=\"post\"> Wie ist dein Name? <input type=\"text\" name=\"firstname\">");
-        response.append("<br> <input type=\"submit\" value=\"Absenden\"> </form></body> </html>");
+        response.append("<head><title>Hallo ");
+        String name = req.getParameter("firstname");
+        if (name == null) {
+            response.append("du da");
+        } else {
+            response.append(name);
+        }
+        response.append("</title></head>");
+        response.append("<body><form method=\"post\"> Wie ist dein Name? <input type=\"text\" name=\"firstname\">");
+        response.append("<br> <input type=\"submit\" value=\"Absenden\"> </form></body>");
         printWriter.print(response);
     }
 
@@ -43,6 +36,5 @@ public class HalloDuDa extends HttpServlet {
     public void init(ServletConfig config) throws ServletException {
         super.init(config);
         System.out.println(this.hashCode() + " || " + getServletContext().hashCode());
-        System.out.println(this.getClass().getCanonicalName());
     }
 }
